7ceb00bbf3d7839e4762c57d017b92e94d5a2f65
Tony Klapatch <tonyklapatch@hotmail.com>
2019-03-28 21:27:33 -0400

    Fix namespace change on update

--- _build/resolvers/loadmodules.resolver.php
+++ _build/resolvers/loadmodules.resolver.php
@@ -2,12 +2,26 @@
 /* @var modX $modx */
 
 if ($object->xpdo) {
+	$modx =& $object->xpdo;
+
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_UPGRADE:
         case xPDOTransport::ACTION_INSTALL:
-            $modx =& $object->xpdo;
 
             $modx->log(modX::LOG_LEVEL_INFO, 'Loading/updating available modules...');
+
+            $modx->log(modX::LOG_LEVEL_INFO, 'Checking for old namespace...');
+            $oldModule = $modx->getObject('comModule', ['class_name' => 'RogueClarity\PrintOrder\Modules\PrintOrder']);
+
+        	// Modify old module information to avoid errors
+            if ($oldModule) {
+                $oldModule->set('class_name', 'PoconoSewVac\PrintOrder\Modules\PrintOrder');
+                $oldModule->set('author', 'Tony Klapatch');
+                $oldModule->save();
+                $modx->log(modX::LOG_LEVEL_INFO, 'Old namespace updated');
+            } else {
+                $modx->log(modX::LOG_LEVEL_INFO, 'Namespace is up to date!');
+            }
 
             $corePath = $modx->getOption('commerce.core_path', null, $modx->getOption('core_path') . 'components/commerce/');
             $commerce = $modx->getService('commerce', 'Commerce', $corePath . 'model/commerce/' , ['isSetup' => true]);
